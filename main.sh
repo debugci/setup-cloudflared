589dd80cb987fadefcfb8011e9678420e5e5118b
Jacob Hummer <jcbhmr@outlook.com>
2023-07-27 15:16:41 -0500

    see if this works

--- main.sh
+++ main.sh
@@ -8,12 +8,23 @@ if [[ $RUNNER_OS == Linux ]]; then
     echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared jammy main' | sudo tee /etc/apt/sources.list.d/cloudflared.list
     sudo apt-get update && sudo apt-get install cloudflared
   else
-    # gh release download -R cloudflare/cloudflared "v$INPUT_CLOUDFLARED_VERSION"
-    # chmod +x cloudflared
-    # sudo mv cloudflared /usr/local/bin/cloudflared
+    gh release download -R cloudflare/cloudflared "$INPUT_CLOUDFLARED_VERSION" -p cloudflared-linux-amd64
+    chmod +x cloudflared-linux-amd64
+    sudo mv cloudflared-linux-amd64 /usr/local/bin/cloudflared
   fi
 elif [[ $RUNNER_OS == Windows ]]; then
-  # cmd.exe ""
+  if [[ $INPUT_CLOUDFLARED_VERSION == latest ]]; then
+    gh release download -R cloudflare/cloudflared -p cloudflared-windows-amd64.msi
+  else
+    gh release download -R cloudflare/cloudflared "$INPUT_CLOUDFLARED_VERSION" -p cloudflared-windows-amd64.msi
+  fi
+  sudo ./cloudflared-windows-amd64.msi
 elif [[ $RUNNER_OS == macOS ]]; then
-  brew install cloudflare/cloudflare/cloudflared
+  if [[ $INPUT_CLOUDFLARED_VERSION == latest ]]; then
+    brew install cloudflare/cloudflare/cloudflared
+  else
+    gh release download -R cloudflare/cloudflared "$INPUT_CLOUDFLARED_VERSION" -p cloudflared-darwin-amd64.tgz
+    tar -xzvf cloudflared-darwin-amd64.tgz
+    sudo mv cloudflared /usr/local/bin/cloudflared
+  fi
 fi
